73f4c720e6a0326d51c0caea1bd9ba727dff4fac
dragonmux <691140+dragonmux@users.noreply.github.com>
2022-12-28 17:55:45 +0000

    Deploying to gh-pages from @ shrine-maiden-heavy-industries/sol@b95408c7cf0a6d907a462baa670967ebb0c10de4 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev185',
+    VERSION: '0.2.1.dev10',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
